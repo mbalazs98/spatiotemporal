a8d81dc99d6a1dd7db74b6cbbd64dd61e8da4bd1
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2024-10-21 14:03:47 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from datasets import RawHD_dataloaders, DVSGesture_dataloaders
+from best_config_RawHD import Config
+from snn_delays import SnnDelays
+import torch
+import utils
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"\n=====> Device = {device} \n\n")
+
+config = Config()
+
+model = SnnDelays(config).to(device)
+
+if config.model_type == 'snn_delays_lr0':
+    model.round_pos()
+
+
+print(f"===> Dataset    = {config.dataset}")
+print(f"===> Model type = {config.model_type}")
+print(f"===> Model size = {utils.count_parameters(model)}\n\n")
+
+
+if config.dataset == 'dvsgesture':
+    train_loader, valid_loader = DVSGesture_dataloaders(config)
+    test_loader = None
+elif config.dataset == 'rawhd':
+    train_loader, valid_loader = RawHD_dataloaders(config)
+    test_loader = None
+else:
+    raise Exception(f'dataset {config.dataset} not implemented')
+
+
+model.train_model(train_loader, valid_loader, test_loader, device)
