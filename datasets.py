7f2fbfb9848c0de91f1d78498b343da35ceb25b5
mbalazs98 <49280623+mbalazs98@users.noreply.github.com>
2024-11-04 09:46:02 +0000

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -5,9 +5,6 @@ import numpy as np
 from torch.utils.data import DataLoader, Dataset
 import torch
 
-import tonic
-import tonic.transforms as transforms
-
         
 def RawHD_dataloaders(config):
     set_seed(config.seed)
@@ -36,17 +33,3 @@ class RawHD(Dataset):
         x = self.x_data[idx]
         y = self.y_data[idx]
         return torch.tensor(x, dtype=torch.float32), torch.tensor(y)
-    
-
-def DVSGesture_dataloaders(config):
-    set_seed(config.seed)
-    sensor_size = tonic.datasets.DVSGesture.sensor_size
-    frame_transform = transforms.ToFrame(sensor_size=sensor_size, n_time_bins=80)
-    transform = transforms.Compose([frame_transform])
-    train_dataset = tonic.datasets.DVSGesture(save_to=config.datasets_path, train=True, transform=transform)
-    test_dataset = tonic.datasets.DVSGesture(save_to=config.datasets_path, train=True, transform=transform)
-
-    train_loader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=4)
-    test_loader = DataLoader(test_dataset, batch_size=config.batch_size, num_workers=4)
-    
-    return train_loader, test_loader
